36500ed66c24c5d06ed72157ccec06c2e3241322
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-11 01:52:37 +0530

    feat: update ui (#1)

--- App.tsx
+++ App.tsx
@@ -5,56 +5,52 @@ import "react-native-get-random-values";
 import "@ethersproject/shims";
 
 // Step 3: Import the thirdweb SDK
-import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm";
 
-import type { NFT } from "@thirdweb-dev/sdk";
 import { StatusBar } from "expo-status-bar";
 import {
+  Button,
+  Linking,
+  SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
-  FlatList,
-  Image,
-  SafeAreaView,
 } from "react-native";
-import { useState, useEffect } from "react";
+import Card from "./components/Card";
 
 export default function App() {
-  const [nfts, setNFTS] = useState<NFT[]>([]);
-  useEffect(() => {
-    const sdk = new ThirdwebSDK("goerli");
-    const loadNFTS = async () => {
-      const contract = await sdk.getContract(
-        "0x2e01763fA0e15e07294D74B63cE4b526B321E389"
-      );
-      return await contract.erc721.getAll();
-    };
-
-    loadNFTS().then((_nfts) => {
-      setNFTS(_nfts);
-    });
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        style={styles.nftList}
-        data={nfts}
-        bounces={true}
-        keyExtractor={(item) => item.metadata.id}
-        renderItem={({ item }) => (
-          <View style={styles.nftItem} key={item.metadata.id}>
-            {item.metadata.image && (
-              <Image
-                style={styles.nftImage}
-                source={{ uri: item.metadata.image }}
-              />
-            )}
-            <Text>{item.metadata.name}</Text>
-          </View>
-        )}
-      />
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => Linking.openURL("https://thirdweb.com")}
+        >
+          <Text style={styles.title}>Welcome to thirdweb</Text>
+        </TouchableOpacity>
+
+        <View style={styles.grid}>
+          <Card
+            description="Guides, references and resources that will help you build with
+                thirdweb."
+            link="https://portal.thirdweb.com"
+            title="Portal"
+          />
+          <Card
+            description="Deploy, configure and manage your smart contracts from the
+                dashboard."
+            link="https://thirdweb.com/dashboard"
+            title="Dashboard"
+          />
+
+          <Card
+            description="Discover and clone template projects showcasing thirdweb
+                features."
+            link="https://portal.thirdweb.com/templates"
+            title="Templates"
+          />
+        </View>
+      </View>
+      <StatusBar style="light" />
     </SafeAreaView>
   );
 }
@@ -62,23 +58,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#0f1318",
     alignItems: "center",
     justifyContent: "center",
   },
-  nftList: {
-    flex: 1,
-    width: "100%",
-    paddingHorizontal: 20,
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#f213a4",
   },
-  nftItem: {
-    width: "100%",
-    flex: 1,
+  grid: {
+    display: "flex",
+    justifyContent: "center",
     alignItems: "center",
-    flexDirection: "row",
-  },
-  nftImage: {
-    width: 100,
-    height: 100,
+    maxWidth: "100%",
+    marginTop: 16,
   },
 });
