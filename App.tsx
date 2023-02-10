3fa567660674b243167b1676958f00d74e17b07e
ikethirdweb <jorge@thirdweb.com>
2023-02-10 15:58:17 -0500

    latest

--- App.tsx
+++ App.tsx
@@ -1,95 +1,46 @@
 /**
- * Sample React Native App using @thirdweb-dev/react-native
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
  *
  * @format
  */
 
+import React from 'react';
+import {Button, Linking, SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import {useContract, useSDK} from '@thirdweb-dev/react-core';
 import {
-  ChainId,
   ThirdwebProvider,
-  useContract,
-  useNFTs,
   useAccount,
-  useThirdwebProvider,
-  useDisconnect,
   useWalletConnect,
-  useSDK,
+  useWalletDisconnect,
 } from '@thirdweb-dev/react-native';
-import React, {useEffect} from 'react';
-import {
-  ActivityIndicator,
-  Image,
-  Linking,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-
-const LOG_TAG = '(TW)';
-const WALLET_CONNECT_PREFIX = 'wc://';
-
-const log = (message: string, ...args: any[]) => {
-  console.log(LOG_TAG, message, ...args);
-};
-
-const deepLink = (uri: string) => {
-  // Tricking the app to open the wallet app. This is usually not needed since
-  // apps should pop up a notification for signing but Trust Wallet does not at the moment.
-  Linking.openURL(uri);
-};
 
 const App = () => {
   return (
-    <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
+    <ThirdwebProvider>
       <AppInner />
     </ThirdwebProvider>
   );
 };
 
 const AppInner = () => {
-  const {isInitializing} = useThirdwebProvider();
-
-  const {connect, error, isLoading, isSuccess, displayUri, connectorError} =
-    useWalletConnect();
-
-  const {isConnected, connector} = useAccount();
-
-  const disconnect = useDisconnect();
-
   const sdk = useSDK();
 
-  useEffect(() => {
-    if (displayUri && !isConnected) {
-      deepLink(displayUri);
-    }
-  }, [displayUri, isConnected]);
+  const {contract} = useContract('0xb8A3454db7042Ee72C93b42565357A2e13967FD4');
 
-  useEffect(() => {
-    if (connectorError) {
-      log('connectorError', connectorError);
-    }
-  }, [connectorError]);
+  const {connector, connect, isLoading, isSuccess, connectError, displayUri} =
+    useWalletConnect();
 
-  const {
-    contract,
-    isLoading: isLoadingContract,
-    error: contractError,
-  } = useContract('0xb8A3454db7042Ee72C93b42565357A2e13967FD4', 'nft-drop');
+  const disconnect = useWalletDisconnect();
 
-  const {
-    data: nfts,
-    isLoading: isLoadingNFTs,
-    error: nftsError,
-  } = useNFTs(contract, {
-    start: 0,
-    count: 100,
-  });
+  const {address: account} = useAccount();
 
-  const onConnectPress = async () => {
-    if (isConnected) {
+  const onPress = () => {
+    if (account) {
       disconnect();
     } else {
       connect();
@@ -97,123 +48,86 @@ const AppInner = () => {
   };
 
   const signMessage = () => {
+    console.log('sign.Message');
+
     sdk?.wallet
       .sign('Test Message')
       .then(tx => {
-        log('response', tx);
+        console.log('response', tx);
       })
-      .catch(signError => log('sign.error', signError));
+      .catch(error => console.log('sign.error', error));
 
-    deepLink(WALLET_CONNECT_PREFIX);
+    Linking.openURL('wc:');
   };
 
-  const claim = () => {
-    if (!contract) {
-      return;
-    }
-
-    contract.erc721
-      .claim(1)
+  const claim = async () => {
+    console.log('claim');
+    contract?.erc721
+      .claimTo('0x0beECa30ea02FB3B6258e056d8d6Cff6fB7d7240', 1)
       .then(tx => {
-        log('tx', tx);
+        console.log('tx', tx);
       })
-      .catch(claimError => {
-        log('claim.error', claimError);
+      .catch(error => {
+        console.log('sendTransaction.error', error);
       });
 
-    deepLink(WALLET_CONNECT_PREFIX);
+    Linking.openURL('wc:');
   };
 
-  if (isInitializing) {
-    return <ActivityIndicator />;
-  }
-
   return (
-    <SafeAreaView style={styles.mainView}>
-      <Text style={styles.title}>Welcome to thirdweb</Text>
-      <TouchableOpacity style={styles.button} onPress={onConnectPress}>
-        <Text style={styles.text}>
-          {isConnected ? 'Disconnect' : 'Connect'}
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={signMessage}>
-        <Text style={styles.text}>Sign Message</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={claim}>
-        <Text style={styles.text}>Claim NFT</Text>
-      </TouchableOpacity>
-      <View style={styles.scrollViewContainer}>
-        {isLoadingNFTs ? <ActivityIndicator /> : null}
-        <ScrollView>
-          {nfts && nfts.length > 0
-            ? nfts.map(nft => {
-                return (
-                  <TouchableOpacity
-                    key={nft.metadata.name}
-                    style={styles.nftView}>
-                    <Text style={styles.nftText}>
-                      Name: {nft.metadata.name}
-                    </Text>
-                    {nft?.metadata.image ? (
-                      <Image
-                        style={styles.image}
-                        resizeMode="contain"
-                        source={{uri: nft?.metadata.image}}
-                      />
-                    ) : null}
-                  </TouchableOpacity>
-                );
-              })
-            : null}
-        </ScrollView>
-      </View>
+    <SafeAreaView style={styles.backgroundStyle}>
+      <Text>{`Account: ${account}`}</Text>
+
+      <Button title={account ? 'Disconnect' : 'Connect'} onPress={onPress} />
+
+      <Button title={'Claim'} onPress={claim} />
+
+      <Button title={'Sign Message'} onPress={signMessage} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  mainView: {
-    paddingTop: 15,
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#0f1318',
-    justifyContent: 'center',
-  },
-  title: {
+  header: {
     fontSize: 20,
     fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#f213a4',
+    marginTop: 10,
   },
   scrollViewContainer: {
     flex: 1,
-    width: '100%',
-    paddingBottom: 20,
+  },
+  touchable: {
+    marginTop: 10,
+    backgroundColor: 'blue',
+    padding: 10,
+    justifyContent: 'center',
+  },
+  connectText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  nftBalance: {
+    marginTop: 10,
+    flex: 1,
   },
   nftView: {
-    alignItems: 'center',
     marginTop: 20,
-  },
-  nftText: {
-    color: 'white',
   },
   image: {
     height: 150,
-    width: '100%',
   },
-  text: {
-    textAlign: 'center',
-    color: '#f213a4',
-  },
-  button: {
-    height: 50,
-    width: 100,
-    margin: 5,
-    alignContent: 'center',
-    justifyContent: 'center',
-    borderColor: 'white',
+  textInput: {
     borderWidth: 0.2,
-    borderRadius: 10,
+    borderRadius: 5,
+    borderColor: 'white',
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  backgroundStyle: {
+    flex: 1,
+    margin: 20,
+    justifyContent: 'space-evenly',
+    alignContent: 'center',
   },
 });
 
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
  * @format
  */
-import 'crypto';
 
 import '@thirdweb-dev/react-native-compat';
 
