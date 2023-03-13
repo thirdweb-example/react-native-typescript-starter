e03f91f295ae1236d38760655297bef2243e069f
ikethirdweb <jorge@thirdweb.com>
2023-03-13 13:44:25 -0400

    remove web3button

--- App.tsx
+++ App.tsx
@@ -8,22 +8,14 @@
  * @format
  */
 
-import {
-  CoinbaseWallet,
-  ConnectWallet,
-  MetaMaskWallet,
-  ThirdwebProvider,
-  Web3Button,
-} from '@thirdweb-dev/react-native';
+import {ConnectWallet, ThirdwebProvider} from '@thirdweb-dev/react-native';
 import React from 'react';
 import {SafeAreaView, StyleSheet, useColorScheme, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   return (
-    <ThirdwebProvider
-      activeChain={'mumbai'}
-      supportedWallets={[MetaMaskWallet, CoinbaseWallet]}>
+    <ThirdwebProvider activeChain={'mumbai'}>
       <AppInner />
     </ThirdwebProvider>
   );
@@ -39,16 +31,6 @@ const AppInner = () => {
     <SafeAreaView style={backgroundStyle}>
       <View style={styles.view}>
         <ConnectWallet />
-
-        <Web3Button
-          contractAddress="0x43e906b1B8bBBE0ba4345B66dc9ae6690E553124"
-          action={async contract => {
-            // a contract action. e.g:
-            const tokenId = 0;
-            const nft = await contract.erc721.get(tokenId);
-          }}>
-          Get all NFTs
-        </Web3Button>
       </View>
     </SafeAreaView>
   );
